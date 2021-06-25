cf556c33afa4c54cddcb42d5d949d2faff6f8714
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-06-25 11:33:33 -0300

    Criação do Adapter (em progresso)

--- app/src/main/java/com/oceanbrasil/ocean_android_listas_multithread_25_06_2021/MainActivity.kt
+++ app/src/main/java/com/oceanbrasil/ocean_android_listas_multithread_25_06_2021/MainActivity.kt
@@ -2,10 +2,19 @@ package com.oceanbrasil.ocean_android_listas_multithread_25_06_2021
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Cada lista precisa de um adapter
+        //LayoutManager existe para estruturar a lista
+
+        val rvLista = findViewById<RecyclerView>(R.id.rvLista)
+        rvLista.layoutManager = LinearLayoutManager(this)
+
     }
 }
